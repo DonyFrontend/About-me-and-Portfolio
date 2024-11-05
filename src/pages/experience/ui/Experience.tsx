f7feea328951fe1fe762d449cecc37e5bc9f1f7d
Doni <donibro357@gmail.com>
2024-11-05 16:26:36 +0600

    add experience

--- src/pages/experience/ui/Experience.tsx
+++ src/pages/experience/ui/Experience.tsx
@@ -6,8 +6,12 @@ import { useTranslation } from "react-i18next";
 
 const Experience = () => {
     const dispatch = useAppDispatch();
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
     const data = useAppSelector(state => state.experience);
+    const C = (a: string, b: string) => {
+        return useC(a, b)
+    }
+
     console.log(data.data);
 
     useEffect(() => {
@@ -15,12 +19,42 @@ const Experience = () => {
     }, [dispatch])
 
     return (
-        <div className="w-full flex justify-center py-20">
-            <main className="componentWidth flex flex-col gap-y-16">
+        <div id="experience" className={`w-full flex ${useC('bg-white', 'bg-[#030712]')} justify-center py-20`}>
+            <main className="componentWidth flex flex-col items-center gap-y-16">
                 <div className="w-full flex flex-col items-center gap-y-4">
-                    <div className={`p-1 px-3 rounded-[12px] ${useC('bg-[#E5E7EB]', 'bg-[#374151]')}`}><p className={`text-[19px] ${useC('text-[#4B5563]', 'text-[#D1D5DB]')}`}>{t('skills')}</p></div>
-                    <p className={`text-[17px] ${useC('text-[#4B5563]', 'text-white')}`}>{t('skills_desc')}</p>
+                    <div className={`p-1 px-3 rounded-[12px] ${useC('bg-[#E5E7EB]', 'bg-[#374151]')}`}><p className={`text-[19px] ${useC('text-[#4B5563]', 'text-[#D1D5DB]')}`}>{t('experience')}</p></div>
+                    <p className={`text-[17px] ${useC('text-[#4B5563]', 'text-white')}`}>{t('experience_desc')}</p>
                 </div>
+
+                <section className="w-[80%] flex flex-col gap-y-10">
+                    {data.data.map((item, index) =>
+                        <article key={index} className={`w-full hover:translate-y-[-15px] transition-all shadow-[10px 5px 5px red] ${C('shadow-cardShadowLight border-[3px]', 'shadow-cardShadowDark')}  rounded-[12px] flex flex-col gap-y-5 p-7 ${C('bg-[white]', 'bg-[#1F2937]')}`}>
+                            <div className="flex justify-between">
+                                <p className="text-[30px] font-semibold">{i18n.language == 'ru' ? item.title.ru : item.title.en}</p>
+                                <p className={` text-[18px] ${C('text-[#4B5563]', 'text-[#D1D5DB]')} `}>{i18n.language == 'ru' ? item.date.ru : item.date.en}</p>
+                            </div>
+                            <div className="w-[100%] flex flex-col items-start gap-y-7">
+                                <div className="flex flex-col w-[70%] gap-y-3">
+                                    <p className="text-[20px] font-medium">{t('project_desc')}</p>
+                                    <p className={`text-[19px] ${C('text-[#4B5563]', 'text-[#dbd1db]')}`}>{i18n.language == 'ru' ? item.description.ru : item.description.en}</p>
+                                </div>
+                                <div className="flex flex-col gap-y-3">
+                                    <p className="text-[20px] font-medium">{t('my_tasks')}</p>
+
+                                    <div className="flex flex-col gap-y-[6px] items-start justify-center">
+                                        {item.tasks.map((item, index) =>
+                                            <div className="flex gap-x-2 items-center">
+                                                <span className={`w-[8px] h-[8px] rounded-full ${C('bg-[#4B5563]', 'bg-[#D1D5DB]')}`}></span>
+                                                <p key={index} className={`text-[19px] ${C('text-[#4B5563]', 'text-[#D1D5DB]')}`}>{i18n.language == 'ru' ? item.ru : item.en}</p>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                                <div></div>
+                            </div>
+                        </article>
+                    )}
+                </section>
             </main>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'cardShadowDark': '10px 7px 5px rgba(112, 28, 119, 0.8)',
+        'cardShadowLight': '8px 5px 5px gray',
+      }
+    },
   },
   plugins: [],
 }
