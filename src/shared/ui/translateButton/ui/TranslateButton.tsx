9dd2cb21956839921b808ea8c003ec5429661afb
Doni <donibro357@gmail.com>
2024-11-08 11:27:56 +0600

    change language button

--- src/shared/ui/translateButton/ui/TranslateButton.tsx
+++ src/shared/ui/translateButton/ui/TranslateButton.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
-import image from '@/shared/assets/icons/Language.svg';
+import langauage_dark from '@/shared/assets/icons/Language.svg';
+import langauage_light from '@/shared/assets/icons/language_light.svg';
 import { useC } from "@/shared/hooks/use-change-theme";
 
 const TranslateButton = () => {
@@ -15,9 +16,9 @@ const TranslateButton = () => {
   ]
   
   return (
-    <div className={`p-3 border-white ${useC('bg-gray-900', 'bg-[#1A0B2E]')} border rounded-[500px] flex gap-x-2 px-4 py-2 items-center`}>
-      <img src={image} alt="Error!" />
-      <select onChange={e => handleChangeLanguage(e.currentTarget.value)} className="bg-[#1A0B2E] text-white text-lg cursor-pointer">
+    <div className={`p-3 ${useC('border-black', 'border-white')} ${useC('bg-white', 'bg-[#1A0B2E]')} border rounded-[500px] flex gap-x-2 px-4 py-2 items-center`}>
+      <img src={useC(langauage_light, langauage_dark)} alt="Error!" />
+      <select onChange={e => handleChangeLanguage(e.currentTarget.value)} className={`${useC('bg-white', 'bg-[#1A0B2E]')} text-lg cursor-pointer`}>
         {languages.map((item, index) => <option key={index} value={item.lang}>{item.text}</option>)}
       </select>
     </div>
