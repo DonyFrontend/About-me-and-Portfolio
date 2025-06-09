e0c42bc340f97fadaf808458c3b2fc09b0994a16
Doni <donibro357@gmail.com>
2025-06-09 21:51:06 +0600

    Change CV link

--- src/shared/ui/CVButton/ui/CVButton.tsx
+++ src/shared/ui/CVButton/ui/CVButton.tsx
@@ -1,13 +1,20 @@
 import { useC } from "@/shared/hooks/use-change-theme";
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
 
 const CVButton = () => {
-  const {t} = useTranslation();
-    return (
-    <a target='_blank' href='https://drive.google.com/file/d/1JySxYClqoks_DQsSVmt9a5iCxdEqdlcE/view?usp=sharing' className={`${useC('text-white', 'text-black')} ${useC('bg-gray-900', 'bg-white')} text-nowrap p-2 px-5 rounded-[12px]`}>
+  const { t } = useTranslation();
+  return (
+    <a
+      target="_blank"
+      href="https://drive.google.com/file/d/1SbxT99_M9xPEUu6GiyE0C28G7D0ZHimD/view?usp=sharing"
+      className={`${useC("text-white", "text-black")} ${useC(
+        "bg-gray-900",
+        "bg-white"
+      )} text-nowrap p-2 px-5 rounded-[12px]`}
+    >
       {t("CV")}
     </a>
-  )
-}
+  );
+};
 
-export default CVButton
+export default CVButton;
